08a377e196192095d39b2eb38a3dc4ac66e260f3
Jorge Ruiz <jorgelinirg@gmail.com>
2021-10-11 18:45:18 +0200

    Ni idea, no me deja hacer pull

--- Práctica 01 - UBUSECRET/UBUSecret/Interfaces/ICapaDatos.cs
+++ Práctica 01 - UBUSECRET/UBUSecret/Interfaces/ICapaDatos.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using UBUSecret;
 namespace Interfaces
 {
-    interface ICapaDatos
+    public interface ICapaDatos
     {
+        public Usuario LeerUsuario(Usuario usuario);
+        public Usuario LeerUsuario(string email);
+
+        public Usuario BorrarUsuario(string email);
+
+        public bool InsertarUsuario(Usuario usuario);
+
+        public List<Usuario> LeerUsuariosDeshabilitados();
+
+        public Secreto BorrarSecreto(string idSecreto);
+        public bool InsertarSecreto(Secreto secreto);
+
+        public List<Secreto> LeerSecretosRecibidos(Usuario usuario);
+        public List<Secreto> LeerSecretosEnviados(Usuario usuario);
+            
     }
 }
