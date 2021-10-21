e42bca448df7ad857119197a95520f70cac31c05
Jorge Ruiz <jorgelinirg@gmail.com>
2021-10-21 02:06:14 +0200

    Listado de Secretos

--- Práctica 01 - UBUSECRET/UBUSecret/Web/secretos.aspx.cs
+++ Práctica 01 - UBUSECRET/UBUSecret/Web/secretos.aspx.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using UBUSecret;
 namespace Web
 {
     public partial class secretos : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Usuario usuario = (Usuario)Session["usuario"];
 
+            if (usuario == null)
+            {
+                Response.Redirect("default.aspx");
+            }
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Secretos.InnerHtml += String.Format(@"<section class=""alert alert-secondary flex flex-row overflow-hidden"" role=""alert"">
+                <div style=""width: 60%"" class=""overflow-hidden truncate "">
+                    {0}
+                </div>
+                <div style=""width: 20%"" class=""overflow-hidden truncate text-center "" >
+                    {1}
+                </div>
+                <div style=""width: 20%"" class=""overflow-hidden truncate text-center"">
+                    {2}
+                </div>
+            </section>", "", "Don Ilitri", "Hoy ");
+                
+            }
         }
     }
 }
