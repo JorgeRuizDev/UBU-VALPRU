bcf4223c16039de2b06d0af299b657eeba2a43e8
axelrg <63260510+axelrg@users.noreply.github.com>
2021-11-03 20:31:39 +0100

    Secreto test acabado

--- Práctica 01 - UBUSECRET/UBUSecret/UBUSecretTests/SecretoTests.cs
+++ Práctica 01 - UBUSECRET/UBUSecret/UBUSecretTests/SecretoTests.cs
@@ -9,28 +9,82 @@ namespace UBUSecret.Tests
     [TestClass()]
     public class SecretoTests
     {
+
+        private Usuario remitente;
+
+        private Usuario receptorA;
+        private Usuario receptorB;
+        private Usuario receptorC;
+        private Usuario receptorD;
+
+        private LinkedList<Usuario> receptores;
+        private Secreto secreto;
+
+        [TestInitialize()]
+        public void Inicializacion()
+        {
+            this.remitente = new Usuario("Pedro", "Piqueras", "pedro@ubu.es", "631 882 831", "1234");
+
+            receptorA = new Usuario("Fernando", "Pozo", "fer@ubu.es", "631 452 831", "1222");
+            receptores.AddLast(receptorA);
+
+            receptorB = new Usuario("Mariano", "Espada", "marian@ubu.es", "631 765 831", "1232");
+            receptores.AddLast(receptorB);
+
+            receptorC = new Usuario("Pepe", "Lobato", "pepe@ubu.es", "631 098 831", "1216");
+            receptores.AddLast(receptorC);
+
+            receptorD = new Usuario("NoEstoy", "Lobato", "pepe@ubu.es", "631 098 831", "1216");
+
+            secreto = new Secreto(remitente, receptores, "Hola este es el mensaje", "HOLA", "nosoyyo");
+
+        }
+
+
         [TestMethod()]
         public void SecretoTest()
         {
-            Assert.Fail();
+            new Secreto(remitente, receptores, "Hola este es el mensaje", "HOLA", "nosoyyo");
         }
 
         [TestMethod()]
         public void TieneAccesoTest()
         {
-            Assert.Fail();
+            //Comprobamos que si tiene acceso un usuario añadido a la lista
+            Assert.IsTrue(secreto.TieneAcceso(receptorC));
+
+            //Comprobamos que no tiene acceso un usuario que no ha sido añadido
+            Assert.IsFalse(secreto.TieneAcceso(receptorD));
         }
 
         [TestMethod()]
         public void DarAccesoTest()
         {
-            Assert.Fail();
+            //Comprobamos que el usuario no tiene acceso
+            Assert.IsFalse(secreto.TieneAcceso(receptorD));
+
+            //Le damos acceso
+            secreto.DarAcceso(receptorD);
+
+            //Comprobamos que ahora si tiene acceso
+            Assert.IsTrue(secreto.TieneAcceso(receptorD));
+
         }
 
         [TestMethod()]
         public void QuitarAccesoTest()
         {
-            Assert.Fail();
+            //Damos acceso al usuario
+            DarAccesoTest();
+
+            //Quitamos acceso al usuario
+            secreto.QuitarAcceso(receptorD);
+
+            //Comprobamos que el usuario no tiene acceso
+            Assert.IsFalse(secreto.TieneAcceso(receptorD));
+            
+
+           
         }
     }
 }
