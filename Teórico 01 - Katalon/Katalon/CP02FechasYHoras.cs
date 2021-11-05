484f97a092e6e3ecb396a6eeb29cd912c706b3a4
Jorge <jrg1011@alu.ubu.es>
2021-11-05 16:43:28 +0100

    Actualizado con sleeps

--- Teórico 01 - Katalon/Katalon/CP02FechasYHoras.cs
+++ Teórico 01 - Katalon/Katalon/CP02FechasYHoras.cs
@@ -64,33 +64,33 @@ namespace SeleniumTests
 
             //Reseteamos el formulario
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div[3]/div/span/span")).Click();
-            Thread.Sleep(300);
+            Thread.Sleep(1000);
             driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Cancelar'])[2]/following::span[2]")).Click();
-            Thread.Sleep(300);
+            Thread.Sleep(1000);
 
             //Clic en si consumimos colacao
             driver.FindElement(By.XPath("//div[@id='i5']/div[3]/div")).Click();
-            
+            Thread.Sleep(1000);
             //Clic en continuar
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div/span/span")).Click();
-            
+            Thread.Sleep(1000);
             //Introducimos 23 en el dia
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/input")).Click();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/input")).Clear();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/input")).SendKeys("23");
-            
+            Thread.Sleep(1000);
             //Introducimos 22 en el mes
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[3]/div/div[2]/div/div/div/input")).Click();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[3]/div/div[2]/div/div/div/input")).Clear();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[3]/div/div[2]/div/div/div/input")).SendKeys("22");
-            
+            Thread.Sleep(1000);
             //Introducimos 1999 en el año
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[5]/div/div[2]/div/div/div/input")).Click();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[5]/div/div[2]/div/div/div/input")).Clear();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[5]/div/div[2]/div/div/div/input")).SendKeys("1999");
-
+            Thread.Sleep(1000);
             //Comprobamos que aparezca el mensaje fecha no valida
-            Thread.Sleep(300);
+            Thread.Sleep(1000);
             driver.FindElement(By.Id("i30")).Click();
             try
             {
@@ -103,41 +103,41 @@ namespace SeleniumTests
 
             //Reseteamos el formulario
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div[3]/div/span/span")).Click();
-            Thread.Sleep(300);
+            Thread.Sleep(1000);
             driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Cancelar'])[2]/following::span[2]")).Click();
-            Thread.Sleep(300);
-
+            Thread.Sleep(1000);
+            
             //Clic en si consumimos colacao
             driver.FindElement(By.XPath("//div[@id='i5']/div[3]/div")).Click();
-
+            Thread.Sleep(1000);
             //Clic en continuar
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div/span/span")).Click();
-
+            Thread.Sleep(1000);
             //Introducimos 23 en el dia
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/input")).Click();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/input")).Clear();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/input")).SendKeys("23");
-
+            Thread.Sleep(1000);
             //Introducimos 03 en el mes
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[3]/div/div[2]/div/div/div/input")).Click();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[3]/div/div[2]/div/div/div/input")).Clear();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[3]/div/div[2]/div/div/div/input")).SendKeys("03");
-            
+            Thread.Sleep(1000);
             //Introducimos 1999 en el año
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[5]/div/div[2]/div/div/div/input")).Click();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[5]/div/div[2]/div/div/div/input")).Clear();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div[5]/div/div[2]/div/div/div/input")).SendKeys("1999");
-
+            Thread.Sleep(1000);
             //Introducimos 33 en la hora
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div/input")).Click();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div/input")).Clear();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div/input")).SendKeys("33");
-
+            Thread.Sleep(1000);
             //Introducimos 33 en el minuto
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div/div/div[3]/div/div/div/div/input")).Click();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div/div/div[3]/div/div/div/div/input")).Clear();
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div/div/div[3]/div/div/div/div/input")).SendKeys("33");
-            
+            Thread.Sleep(1000);
             //Comprobamos que aparezca un mensaje de hora incorrecta
             driver.FindElement(By.XPath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div")).Click();
             try
