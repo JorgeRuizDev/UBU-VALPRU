19121920628a13b733972069eb1cd99c164f310f
axelrg <63260510+axelrg@users.noreply.github.com>
2021-11-06 00:46:58 +0100

    eliminar codigo innecesario

--- Teórico 01 - Katalon/Katalon/CP02FechasYHoras.cs
+++ Teórico 01 - Katalon/Katalon/CP02FechasYHoras.cs
@@ -151,46 +151,8 @@ namespace SeleniumTests
         
         }
 
+        }
 
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-        
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-        
-        private string CloseAlertAndGetItsText() {
-            try {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert) {
-                    alert.Accept();
-                } else {
-                    alert.Dismiss();
-                }
-                return alertText;
-            } finally {
-                acceptNextAlert = true;
-            }
-        }
+
     }
 }
